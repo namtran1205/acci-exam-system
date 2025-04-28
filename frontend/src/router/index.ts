18ab9d54433599661e5fb322da3e8c7920faa029
Nguyệt Ánh <128211112+hikawi@users.noreply.github.com>
2025-04-28 07:48:01 +0700

    Layout (#4)
    * remove caching
    
    * fix lockfiles
    
    * setup building for playwright
    
    * fix routing and prebuild for playwright
    
    * install playwright with deps
    
    * add layout and remove default vue stuff
    
    * login example layout
    
    * remove all traces of vitest
    
    * fix stupid css missing }

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,23 +1,21 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import LoginView from "@/views/LoginView.vue";
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: "/",
+      name: "ACCI | Login",
+      component: LoginView,
     },
   ],
-})
+});
 
-export default router
+// Change the page title on navigation.
+router.beforeEach((to, from, next) => {
+  document.title = to.name?.toString() || "ACCI | Undefined";
+  next();
+});
+
+export default router;
---
+++ frontend/src/views/LoginView.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import TextField from "@/components/TextField.vue";
+import { ref } from "vue";
+
+const username = ref("Username");
+const password = ref("");
+
+function hello() {
+  console.log(`${username.value}, ${password.value}`);
+}
+</script>
+
+<template>
+  <div
+    class="border-live-olive bg-almost-white mt-32 flex w-full flex-col items-center justify-center gap-2.5 rounded-2xl border-[1px] px-4 py-8 duration-200 lg:gap-12 lg:px-8 lg:py-12"
+  >
+    <h1 class="text-2xl font-bold">Sign In</h1>
+
+    <div class="flex w-full flex-col gap-4">
+      <TextField label="Employee ID" input="text" v-model="username" />
+      <TextField label="Password" input="password" v-model="password" />
+    </div>
+
+    <div class="flex w-full flex-col gap-4">
+      <button
+        class="bg-moss hover:bg-leaf h-16 w-full cursor-pointer rounded-lg font-bold tracking-widest text-white duration-200"
+        @click="hello"
+      >
+        LOGIN
+      </button>
+    </div>
+  </div>
+</template>
