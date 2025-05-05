0a1a159de03841ed7066d6f71c7cac92b5eaa480
tsukimi <128211112+hikawi@users.noreply.github.com>
2025-05-05 13:17:23 +0700

    im dying

--- backend/src/services/participants.service.ts
+++ backend/src/services/participants.service.ts
@@ -1,4 +1,6 @@
+import { asc, eq, sql } from "drizzle-orm";
 import db from "../db/connection";
+import { enrollments } from "../models/enrollment";
 import { participants } from "../models/participant";
 
 export async function saveParticipant(data: {
@@ -8,4 +10,20 @@ export async function saveParticipant(data: {
   registrationId: number;
 }) {
   return db.insert(participants).values(data).returning();
+}
+
+export async function getParticipants(registration: number) {
+  return db
+    .select({
+      id: participants.id,
+      name: participants.name,
+      dateOfBirth: participants.dateOfBirth,
+      gender: participants.gender,
+      examinations: sql`count(${enrollments.id})`,
+    })
+    .from(participants)
+    .innerJoin(enrollments, eq(enrollments.participantId, participants.id))
+    .groupBy(participants.id, participants.name, participants.dateOfBirth, participants.gender)
+    .where(eq(participants.registrationId, registration))
+    .orderBy(asc(participants.id));
 }
--- backend/src/services/registrations.service.ts
+++ backend/src/services/registrations.service.ts
@@ -4,11 +4,15 @@ import { customers } from "../models/customer";
 import { registrations } from "../models/registration";
 
 export async function getAllRegistrations() {
-  return db
-    .select()
+  const regs = await db
+    .select({
+      registrations,
+      customer: customers,
+    })
     .from(registrations)
     .innerJoin(customers, eq(registrations.customerId, customers.id))
     .orderBy(asc(registrations.id));
+  return regs;
 }
 
 export async function saveRegistration(data: { customerId: number; billId?: number }) {
